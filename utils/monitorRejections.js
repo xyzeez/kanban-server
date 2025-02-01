ea8e4353c974f030dcb1277a053117a1fc846a84
Abdu-Samii Abdul-Azeez <106898953+xyzeez@users.noreply.github.com>
2025-02-01 11:24:57 +0300

    chore(server): refined server error cli logs

--- utils/monitorRejections.js
+++ utils/monitorRejections.js
@@ -1,13 +1,21 @@
 exports.monitorUncaughtRejection = () => {
   process.on('uncaughtException', (err) => {
-    console.log('UNCAUGHT REJECTION, Shutting Down...');
+    console.log('========== UNCAUGHT EXCEPTION ==========');
+    console.error(err);
+    console.log('========================================');
+    console.log('SERVER IS SHUTTING DOWN...');
+    console.log('========================================');
     process.exit(1);
   });
 };
 
 exports.monitorUnhandledRejection = (server) => {
   process.on('unhandledRejection', (err) => {
-    console.log('UNHANDLED REJECTION, Shutting Down...');
+    console.log('========== UNHANDLED REJECTION ==========');
+    console.error(err);
+    console.log('========================================');
+    console.log('SERVER IS SHUTTING DOWN...');
+    console.log('========================================');
     server.close(() => process.exit(1));
   });
 };
